d4dabb43f350318a76d5d0add3d659fe307c13d6
Vladimir Still <git@vstill.eu>
2020-09-08 16:55:17 +0200

    sql: Fix default privileges

--- sql/common.sql
+++ sql/common.sql
@@ -1,11 +1,16 @@
 create role frag_{POLL}_{COURSE} login;
+grant frag_{POLL}_{COURSE} to current_user with admin option;
 create schema if not exists frag_{POLL};
 grant all on schema frag_{POLL} to frag_{POLL}_{COURSE};
 grant usage on schema frag_{POLL} to {COURSE}_teacher;
-grant select on all tables in schema frag_{POLL} to {COURSE}_teacher;
-alter default privileges in schema frag_{POLL} grant all on tables to current_user with grant option;
-alter default privileges in schema frag_{POLL} grant all on sequences to current_user with grant option;
-alter default privileges in schema frag_{POLL} grant all on types to current_user with grant option;
+alter default privileges for role frag_{POLL}_{COURSE} in schema frag_{POLL} grant all on tables to current_user with grant option;
+alter default privileges for role frag_{POLL}_{COURSE} in schema frag_{POLL} grant all on sequences to current_user with grant option;
+alter default privileges for role frag_{POLL}_{COURSE} in schema frag_{POLL} grant all on functions to current_user with grant option;
+alter default privileges for role frag_{POLL}_{COURSE} in schema frag_{POLL} grant all on types to current_user with grant option;
+alter default privileges for role frag_{POLL}_{COURSE} in schema frag_{POLL} grant select on tables to {COURSE}_teacher;
+alter default privileges for role frag_{POLL}_{COURSE} in schema frag_{POLL} grant usage on sequences to {COURSE}_teacher;
+alter default privileges for role frag_{POLL}_{COURSE} in schema frag_{POLL} grant execute on functions to {COURSE}_teacher;
+alter default privileges for role frag_{POLL}_{COURSE} in schema frag_{POLL} grant usage on types to {COURSE}_teacher;
 
 set search_path to frag;
 
