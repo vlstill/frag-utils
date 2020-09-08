3cac91e9f3de4a273c8034ab56415688ebda60f8
Vladimir Still <git@vstill.eu>
2020-09-08 16:34:10 +0200

    sql: Fix privileges on the poller schema

--- sql/common.sql
+++ sql/common.sql
@@ -1,7 +1,14 @@
 create role frag_{POLL}_{COURSE} login;
 create schema if not exists frag_{POLL};
 grant all on schema frag_{POLL} to frag_{POLL}_{COURSE};
+grant usage on schema frag_{POLL} to {COURSE}_teacher;
+grant select on all tables in schema frag_{POLL} to {COURSE}_teacher;
+alter default privileges in schema frag_{POLL} grant all on tables to current_user with grant option;
+alter default privileges in schema frag_{POLL} grant all on sequences to current_user with grant option;
+alter default privileges in schema frag_{POLL} grant all on types to current_user with grant option;
+
 set search_path to frag;
+
 grant usage on schema frag to frag_{POLL}_{COURSE};
 grant insert, select on submission to frag_{POLL}_{COURSE};
 grant insert, select on submission_in to frag_{POLL}_{COURSE};
